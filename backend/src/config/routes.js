2caece04205af6e6f4ee53d993a619fe63381fec
Leonardoads <leonardodrade@gmail.com>
2020-03-11 17:23:54 -0300

    ToDo

---
+++ backend/src/config/routes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+module.exports = function(server){
+
+    //API ROUTES
+
+    const router = express.Router()
+    server.use('/api',router)
+
+    // TODO routes
+    const todoService = require('../api/todo/todoService')
+    todoService.register(router,'/todos')
+
+}
+
